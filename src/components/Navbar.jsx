6bf0fa74d55090f3f022092a18c5e4299ed2fc85
Alba Giraldo <agiraldomolina@gmail.com>
2024-06-21 06:48:35 -0400

    fix useSearchTerm bug

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,13 @@
 import NavbarItem from "./NavbarItem";
+import { Suspense } from "react";
 
 export default function Navbar() {
   return (
     <div className="flex dark:bg-gray-700 bg-amber-100 p-4 lg:text-lg justify-center gap-6 ">
+      <Suspense>
         <NavbarItem tittle='Trending' param= 'fetchTrending'/>
         <NavbarItem tittle='Top Rated' param= 'fetchTopRated'/>
+      </Suspense>
     </div>
   )
 }
